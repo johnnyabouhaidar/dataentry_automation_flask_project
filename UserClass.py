f3e6f9a4c5e6aef8c9bfdebc734b60eedcd084a4
johnnyabouhaidar <johnnyabuhaydar@gmail.com>
2022-09-29 17:57:34 +0300

    nothing

--- UserClass.py
+++ UserClass.py
@@ -3,3 +3,4 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     username = db.Column(db.String(50),nullable=False)
     password = db.Column(db.String(50),nullable=False)
+    role = db.Column(db.String(50),nullable=False)
--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+#from UserClass import *
 
 app = Flask(__name__)
 
+
+
 db=SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI']='mssql://johnny:pass123456@localhost\SQLEXPRESS02/Flask_DataEntry_DB'
+app.config['SQLALCHEMY_DATABASE_URI']='mssql+pyodbc://johnny:pass123456@localhost\SQLEXPRESS02/Flask_DataEntry_DB?driver=sql+server?trusted_connection=yes'
 app.config['SECRET_KEY']='thisisasecretkey'
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(50),nullable=False)
+    password = db.Column(db.String(50),nullable=False)
+    role = db.Column(db.String(50),nullable=False)
 
 @app.route('/')
 def home():
