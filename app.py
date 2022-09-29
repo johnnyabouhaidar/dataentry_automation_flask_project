c090b310b1c22d58d1771e3a3cc494de28e12a25
johnnyabouhaidar <johnnyabuhaydar@gmail.com>
2022-09-29 23:40:17 +0300

    prog

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import InputRequired,Length,ValidationError
+from flask_bcrypt import Bcrypt
+
 #from UserClass import *
 
 app = Flask(__name__)
@@ -8,26 +13,62 @@ app = Flask(__name__)
 
 
 db=SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI']='mssql+pyodbc://johnny:pass123456@localhost\SQLEXPRESS02/Flask_DataEntry_DB?driver=sql+server?trusted_connection=yes'
+#app.config['SQLALCHEMY_DATABASE_URI']='mssql+pyodbc://johnny:pass123456@localhost\SQLEXPRESS02/Flask_DataEntry_DB?driver=sql+server?trusted_connection=yes'
+app.config['SQLALCHEMY_DATABASE_URI']=f"mssql+pyodbc://flask1:flaskPass@localhost\SQLEXPRESS/Flask_DataEntry_DB?driver=ODBC+Driver+17+for+SQL+Server"
 app.config['SECRET_KEY']='thisisasecretkey'
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(50),nullable=False)
-    password = db.Column(db.String(50),nullable=False)
+    username = db.Column(db.String(50),nullable=False, unique=True)
+    password = db.Column(db.String(80),nullable=False)
     role = db.Column(db.String(50),nullable=False)
+
+class RegisterForm(FlaskForm):
+    username=StringField(validators=[InputRequired(),Length(min=4,max=20)],render_kw={"placeholder":"Username"})
+    password=PasswordField(validators=[InputRequired(),Length(min=4,max=20)],render_kw={"placeholder":"Password"})
+
+    submit =SubmitField("Register")
+
+    def validate_username(self,username):
+        existing_user_username = User.query.filter_by(
+            username=username.data
+        ).first()
+        if existing_user_username:
+            raise ValidationError(
+                "That username already exists. Please choose a different one."
+            )
+class LoginForm(FlaskForm):
+    username=StringField(validators=[InputRequired(),Length(min=4,max=20)],render_kw={"placeholder":"Username"})
+    password=PasswordField(validators=[InputRequired(),Length(min=4,max=20)],render_kw={"placeholder":"Password"})
+
+    submit=SubmitField("Login")
+
 
 @app.route('/')
 def home():
     return render_template("welcome.html")
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    form=LoginForm()
 
-@app.route('/register')
+
+    return render_template("login.html",form=form)
+
+@app.route('/register',methods=['GET','POST'])
 def register():
-    return render_template("register.html")
+    form=RegisterForm()
+    if form.validate_on_submit():
+
+        password=form.password.data
+        hashed_password= Bcrypt.generate_password_hash(password)
+
+        new_user = User(username=form.username.data,password=hashed_password,role="user")
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template("register.html",form=form)
 
 
 if __name__=='__main__':
